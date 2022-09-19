32993398e08d7286f61b03befc9a6a1dce95d434
Sadique Jemeel Adekunle <sadiqadekunle1@gmail.com>
2022-09-19 03:49:04 -0700

    function that prints the 9 times table, starting with 0.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,6 +11,7 @@ void times_table(void)
 		_putchar('0');
 
 		for (j = 1; j <= 9; j++)
+		{
 			_putchar(',');
 			_putchar(' ');
 
@@ -20,8 +21,9 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((product / 10) + '0');
-			_putchar((product % 10) + '0');
 
+			_putchar((product % 10) + '0');
+		}
 	}
 	_putchar('\n');
 }
