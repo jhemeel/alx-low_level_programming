e00afeb722155b1db575e28cca5db0d47ac1711d
Sadique Jemeel Adekunle <sadiqadekunle1@gmail.com>
2022-09-18 17:44:59 -0700

    a function that checks for alphabetic character.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *_isalpha -unction that checks for alphabetic character.
  *@c: character that is cjecked
@@ -8,8 +7,7 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || 
-	    (c >= 'A' && c <= Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
