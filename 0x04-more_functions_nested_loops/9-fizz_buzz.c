c8733c1c758418b947350b42964af17327df438c
Sadique Jemeel Adekunle <sadiqadekunle1@gmail.com>
2022-09-20 03:53:52 -0700

    FizBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,13 +14,13 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if (num % 15 == 0)
-			printf("FizzBuzz\n");
+			printf("FizzBuzz");
 		else if (num % 5 == 0)
-			printf("Buzz\n");
+			printf("Buzz");
 		else if (num % 3 == 0)
-			printf("Fizz\n");
+			printf("Fizz");
 		else
-			printf("%d\n", num);
+			printf("%d", num);
 
 		if (num == 100)
 			continue;
