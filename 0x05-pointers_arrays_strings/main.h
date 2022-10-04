46609d8c0275b60b70749384dfe788057ad07625
Sadique Jemeel Adekunle <sadiqadekunle1@gmail.com>
2022-10-04 04:50:28 -0700

    function that copies the string pointed to by src, including the terminating null byte (\0),

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,6 +11,8 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void print_array(int *a, int n);
+void puts_half(char *str);
+char *_strcpy(char *dest, char *src);
 
 
 
