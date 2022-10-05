a62465b6de9b039351a8b26aae26e445201de589
Sadique Jemeel Adekunle <sadiqadekunle1@gmail.com>
2022-10-05 10:05:53 -0700

    function that adds two numbers.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,7 +12,7 @@ char *leet(char *s);
 char *rot13(char *s);
 void print_number(int n);
 int _putchar(char c);
-#include <unistd.h>
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
 
